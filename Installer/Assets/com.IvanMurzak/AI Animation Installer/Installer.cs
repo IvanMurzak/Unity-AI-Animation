b2666e253c53676436832b211de9aaba58e86193
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-06-11 18:19:01 -1000

    AI Game Developer update (#33)

--- Installer/Assets/com.IvanMurzak/AI Animation Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI Animation Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.Animation.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.animation";
-        public const string Version = "1.2.18";
+        public const string Version = "1.2.19";
 
         static Installer()
         {
